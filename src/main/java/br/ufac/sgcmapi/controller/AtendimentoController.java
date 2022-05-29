2a49b773cdce64fc94b8f902e7f8c3778b8c4e12
billyraypreacherson <106418695+billyraypreacherson@users.noreply.github.com>
2022-05-29 10:43:26 -0500

    Update AtendimentoController.java

--- src/main/java/br/ufac/sgcmapi/controller/AtendimentoController.java
+++ src/main/java/br/ufac/sgcmapi/controller/AtendimentoController.java
@@ -19,7 +19,7 @@ import br.ufac.sgcmapi.model.Atendimento;
 import br.ufac.sgcmapi.service.AtendimentoService;
 
 @RestController
-@RequestMapping("/atendimentos")
+@RequestMapping("/atendimento")
 public class AtendimentoController implements ICrudController<Atendimento> {
 
     private final AtendimentoService servico;
