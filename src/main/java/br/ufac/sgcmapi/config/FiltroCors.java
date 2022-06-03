b69f85f0c476fd0f41532e192faf5d18efc6a2b5
billyraypreacherson <106418695+billyraypreacherson@users.noreply.github.com>
2022-06-03 17:43:12 -0500

    Update FiltroCors.java

--- src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
+++ src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
@@ -1,6 +1,7 @@
 package br.ufac.sgcmapi.config;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
